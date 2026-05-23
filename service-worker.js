98538fb3eb6c056c8c8aae7fb28fb3ce0e585bec
Santiago Jugo Murcielagas <sjugomurcielagas@gmail.com>
2026-05-23 19:33:18 -0300

    UI mobile + unificacion topbars + fix antidoping + colapsables en reportes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ct-murcielagas-pwa-v2';
+const CACHE_NAME = 'ct-murcielagas-pwa-v3';
 const STATIC_CACHE = `${CACHE_NAME}-static`;
 const RUNTIME_CACHE = `${CACHE_NAME}-runtime`;
 const APP_SHELL = [
